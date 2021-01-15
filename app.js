27b1ee999de11f63e52132021514c630284587ca
s4ng <zalcls951201@gmail.com>
2021-01-15 14:42:14 +0900

    update

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 2049;
+const PORT = 6379;
 
 app.use(bodyParser.json());
 
 const runningShell = () => {
-    
+
 }
 
-app.post("/", (req, res) => {
+app.get("/", (req, res) => {
+    res.send("hello");
+    req.status(200).end()
+})
+
+app.post("/hook", (req, res) => {
     console.log(req.body)
     res.status(200).end()
 })
