fce0cf2efc7f203e01455c596ac33c364b260d86
s4ng <zalcls951201@gmail.com>
2021-01-18 10:08:38 +0900

    add deploy feature

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,4 +1,10 @@
-#!bin/bash
+#!/bin/bash
 PORT=6379
-docker pull s4ng/cicd-dino
-docker run --rm -it --name cicdapp -p $PORT:$PORT s4ng/cicd-dino
+REPOSITORY=s4ng/cicd-dino
+NAME=cicdapp
+
+docker container stop $(docker container ls -q --filter name=$NAME)
+docker rmi $(docker images -q $REPOSITORY)
+docker pull $REPOSITORY
+docker run -d --rm --name $NAME -p $PORT:$PORT $REPOSITORY
+exit 0
